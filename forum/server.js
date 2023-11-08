b66223536bdeef09992a77f9363f3799c92ed43c
yunyoung0531 <yunyoung0531@naver.com>
2023-11-08 22:28:29 +0900

    MongoDB와 서버 연결

--- forum/server.js
+++ forum/server.js
@@ -3,15 +3,28 @@ const app = express()
 
 app.use(express.static(__dirname + '/public'))
 
-app.listen(8080, () => {
-    console.log('http://localhost:8080 에서 서버 실행 중')
+const { MongoClient } = require('mongodb');
+
+let db
+const url = 'mongodb+srv://yunyoung0531:chldbsdud01!@yunyoung0531.5jpytmr.mongodb.net/?retryWrites=true&w=majority';
+new MongoClient(url).connect().then((client)=>{
+    console.log('DB연결성공')
+    db = client.db('forum2')
+    app.listen(8080, () => {
+        console.log('http://localhost:8080 에서 서버 실행 중')
+    })
+}).catch((err)=>{
+    console.log(err)
 })
+
+
 
 app.get('/', (요청, 응답) => {
     응답.sendFile(__dirname + '/index.html')
 })
 
 app.get('/news', (요청, 응답) => {
+    // db.collection('post2').insertOne({title : '어쩌구'})
     응답.send('오늘 비옴')
 })
 
@@ -22,3 +35,4 @@ app.get('/shop', (요청, 응답) => {
 app.get('/about', (요청, 응답) => {
     응답.sendFile(__dirname + '/about.html')
 })
+
